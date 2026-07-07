43773ff9f3ecfefcedeb3f411132c9df229462fa
VladislavSivuha <vladislav.sivuha@gmail.com>
2026-07-07 10:24:46 +0300

    Создание метода Questionnaire (анкета)

--- Homework/Homework/Homework1.cs
+++ Homework/Homework/Homework1.cs
@@ -8,6 +8,30 @@ namespace Homework
 {
     public static class Homework1
     {
+        public static void Questionnaire()
+        {
+            Console.WriteLine("\t\tЗАПОЛНЕНИЕ АНКЕТЫ");
+            
+            Console.Write("Введите ваше имя: ");
+            string name = Console.ReadLine();
+            
+            Console.Write("Введите ваш возраст: ");
+            int age = 0;
+            while (!int.TryParse(Console.ReadLine(), out age))
+            {
+                Console.Write("ОШИБКА! Введите число! ");
+            }
+            
+            Console.Write("Введите ваш рост (в метрах): ");
+            double height = 0;
+            while (!double.TryParse(Console.ReadLine().Replace('.', ','), out height))
+            {
+                Console.Write("ОШИБКА! Введите число! ");
+            }
 
+            Console.WriteLine($"Имя: {name}");
+            Console.WriteLine($"Возраст: {age}");
+            Console.WriteLine($"Рост: {height}");
+        }
     }
 }
