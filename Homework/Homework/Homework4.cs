e72567fbb8f8eb77a09872f598f8e5aba37e23b9
VladislavSivuha <vladislav.sivuha@gmail.com>
2026-07-07 11:05:10 +0300

    - Исправлено зацикливание в методе StringToDouble - Дописан метод FormNTo1

--- Homework/Homework/Homework4.cs
+++ Homework/Homework/Homework4.cs
@@ -10,9 +10,37 @@ namespace Homework
     {
         public static void FormNTo1()
         {
-            Console.WriteLine("Введите число: ");
+            Console.Write("Введите число не равное 1: ");
+            int num = StringToDouble(Console.ReadLine());
 
-
+            if (num == 1)
+            {
+                Console.WriteLine("Число уже равно 1");
+            }
+            else if (num > 1)
+            {
+                for (; num >= 1; num--)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+            else
+            {
+                for (; num <= 1; num++)
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+        static int StringToDouble(string stringNumber)
+        {
+            int num = 0;
+            while (!int.TryParse(stringNumber, out num))
+            {
+                Console.Write("ОШИБКА! Введите число! ");
+                stringNumber = Console.ReadLine();
+            }
+            return num;
         }
     }
 }
