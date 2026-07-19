a834cdee6471bfe5173e2386e0cfaf550a696cd9
VladislavSivuha <vladislav.sivuha@gmail.com>
2026-07-20 00:19:31 +0300

    ДЗ для уроков 6-7

---
+++ Homework/FinanceTracker.CLI/Program.cs
@@ -0,0 +1,14 @@
+﻿using FinanceTracker.Application.Services;
+
+namespace FinanceTracker.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FinanceService service = new FinanceService();
+            Console.WriteLine("Мой финансовый трекер");
+            Console.WriteLine($"Мой баланс: {service.GetTotalIncome() - service.GetTotalExpence()}");
+        }
+    }
+}
