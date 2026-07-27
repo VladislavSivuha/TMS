7c8f3b7112686130bd4cf15b79642cd2f7aa0ccd
VladislavSivuha <vladislav.sivuha@gmail.com>
2026-07-27 11:49:31 +0300

    ДЗ к уроку 8

---
+++ Homework/Homework/Homework8.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework
+{
+    class Homework8
+    {
+
+    }
+
+    abstract class Animal
+    {
+        public string Name { get; set; }
+
+
+        public void SetName(string name)
+        { 
+            Name = name;
+        }
+        public void GetName()
+        {
+            Console.WriteLine($"Имя животного: {Name}");
+        }
+        public abstract void Eat();
+    }
+    class Dog : Animal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine($"Собака ест");
+        }
+    }
+}
